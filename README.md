fd9720b74f9d6e269c16e0a2c40ea773177c8809
matthew-allen2409 <matthew.allen2409@gmail.com>
2024-11-05 23:36:40 -0600

    adding blank branch

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # bit-manipulation-practice
+
+Some practice problems for bit manipulation in rust. Going through these should give you a reasonable grasp of bit manipulation in rust.
+
+## Quickstart
+
+checkout the "blank" branch to get empty function definitions.
+Fill out the functions and run 'cargo test' to check your work.
--- src/lib.rs
+++ src/lib.rs
@@ -1,111 +1,53 @@
-pub fn is_lsb_set(bytes: u32) -> bool {
-    bytes & 0b1 != 0
+pub fn is_lsb_set(_bytes: u32) -> bool {
+    todo!()
 }
 
-pub fn is_msb_set(bytes: u32) -> bool {
-    bytes & (0b1 << 31) != 0
+pub fn is_msb_set(_bytes: u32) -> bool {
+    todo!()
 }
 
-pub fn get_nth_bit(bytes: u32, n: u8) -> bool {
-    if n > 31 {
-        panic!("n can only be in range of 0 - 31");
-    }
-
-    bytes & (0b1 << n) != 0
+pub fn get_nth_bit(_bytes: u32, _n: u8) -> bool {
+    todo!()
 }
 
-pub fn clear_nth_bit(bytes: &mut u32, n: u8) {
-    if n > 31 {
-        panic!("n can only be in range of 0 - 31");
-    }
-
-    *bytes &= !(1 << n);
+pub fn clear_nth_bit(_bytes: &mut u32, _n: u8) {
+    todo!()
 }
 
-pub fn toggle_nth_bit(bytes: &mut u32, n: u8) {
-    if n > 31 {
-        panic!("n can only be in range of 0 - 31");
-    }
-
-    *bytes ^= 1 << n;
+pub fn toggle_nth_bit(_bytes: &mut u32, _n: u8) {
+    todo!()
 }
 
-pub fn get_highest_set_bit(bytes: &u32) -> Option<u8> {
-    for n in (0..32).rev() {
-        if bytes & (1 << n) != 0 {
-            return Some(n);
-        }
-    }
-
-    None
+pub fn get_highest_set_bit(_bytes: &u32) -> Option<u8> {
+    todo!()
 }
 
-pub fn get_lowest_set_bit(bytes: &u32) -> Option<u8> {
-    for n in 0..32 {
-        if bytes & (1 << n) != 0 {
-            return Some(n);
-        }
-    }
-
-    None
+pub fn get_lowest_set_bit(_bytes: &u32) -> Option<u8> {
+    todo!()
 }
 
-pub fn count_trailing_zeroes(bytes: &u32) -> u8 {
-    let mut count = 0;
-
-    for n in 0..32 {
-        if bytes & (1 << n) == 0 {
-            count += 1;
-        } else {
-            break;
-        }
-    }
-
-    count
+pub fn count_trailing_zeroes(_bytes: &u32) -> u8 {
+    todo!()
 }
 
-pub fn count_leading_zeroes(bytes: &u32) -> u8 {
-    let mut count = 0;
-
-    for n in (0..32).rev() {
-        if bytes & (1 << n) == 0 {
-            count += 1;
-        } else {
-            break;
-        }
-    }
-
-    count
+pub fn count_leading_zeroes(_bytes: &u32) -> u8 {
+    todo!()
 }
 
-pub fn flip_all_bits(bytes: &mut u32) {
-    for n in 0..32 {
-        *bytes ^= 1 << n;
-    }
+pub fn flip_all_bits(_bytes: &mut u32) {
+    todo!()
 }
 
-pub fn count(bytes: &u32) -> (u8, u8) {
-    let mut result = (0, 0);
-    for n in 0..32 {
-        if *bytes & (1 << n) != 0 {
-            result.1 += 1;
-        } else {
-            result.0 += 1;
-        }
-    }
-    result
+pub fn count(_bytes: &u32) -> (u8, u8) {
+    todo!()
 }
 
-pub fn rotate_right(bytes: &mut u32, n: u8) {
-    let n = n % 32;
-
-    *bytes = (*bytes >> n) | (*bytes << (32 - n));
+pub fn rotate_right(_bytes: &mut u32, _n: u8) {
+    todo!()
 }
 
-pub fn rotate_left(bytes: &mut u32, n: u8) {
-    let n = n % 32;
-
-    *bytes = (*bytes << n) | (*bytes >> (32 - n));
+pub fn rotate_left(_bytes: &mut u32, _n: u8) {
+    todo!()
 }
 
 #[cfg(test)]
